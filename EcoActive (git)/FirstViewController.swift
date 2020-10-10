f3b15163b33de8384f30de9c680c48e6d53ca40c
Jonah Bard <jonahbard@gmail.com>
2020-10-10 12:07:06 -0700

    Initial Commit

---
+++ EcoActive (git)/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EcoActive (git)
+//
+//  Created by Jonah Bard on 10/10/20.
+//  Copyright © 2020 Jonah Bard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EcoActive (git)/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EcoActive (git)
+//
+//  Created by Jonah Bard on 10/10/20.
+//  Copyright © 2020 Jonah Bard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
